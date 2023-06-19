5e8644a9fdc0ecc46cff5690bb60df2bb9664add
capstone <igor.mestechkin@gmail.com>
2023-06-19 00:05:39 -0400

    completed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 // Note: createStore and candyReducer must be exported for the tests to run
 
-export function createStore() {
-  // write your createStore code here
-}
+// export 
+function createStore(reducer) {
+  let state;
+  
+  function getState () {
+    return state;
+  };
 
-export function candyReducer(state = [], action) {
+  function dispatch (action) {
+    state = reducer(state, action);
+    render();
+  };
+
+  return {
+    getState,
+    dispatch,
+  };
+};
+
+// export 
+function candyReducer(state = [], action) {
   switch (action.type) {
     case "candies/add":
       return [...state, action.candy];
     default:
       return state;
-  }
-}
+  };
+};
+
+let store = createStore(candyReducer);
 
 function render() {
   let container = document.getElementById("container");
@@ -19,8 +37,13 @@ function render() {
     container.textContent = store.getState().join(" ");
   } else {
     throw new Error("the store's state has not been defined yet");
-  }
-}
+  };
+};
 
 // Use your createStore function and the functions provided here to create a store.
 // Once the store is created, call an initial dispatch.
+
+store.dispatch({ 
+  type: 'candies/add',
+  candy: 'milky way bars'
+});
